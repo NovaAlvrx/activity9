2595315c1643904b59623fc1a284c4fb52497b1a
Noah Alvarez <noahalvarezzz16@gmail.com>
2025-10-06 18:42:32 -0400

    Second Page Background

--- lib/second_page.dart
+++ lib/second_page.dart
@@ -5,19 +5,27 @@ class SecondPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Second Page')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Welcome to the second page!', style: TextStyle(fontSize: 24)),
-            SizedBox(height: 20),
-            ElevatedButton(
-              child: Text('Go Back'),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-          ],
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/halloween2.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Welcome to the second page!', style: TextStyle(fontSize: 24)),
+              SizedBox(height: 20),
+              ElevatedButton(
+                child: Text('Go Back'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
