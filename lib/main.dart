7a037926371dfea361f8a11c2c9966a571a1d520
ggiacobbe2 <ggiacobbe13@gmail.com>
2025-10-06 23:29:14 -0400

    finalizing main page UI and game mechanisms and also connecting to second_page

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,23 @@
 import 'package:flutter/material.dart';
 import 'second_page.dart';
+import 'package:flutter_emoji/flutter_emoji.dart';
+import 'dart:async';
+import 'dart:math';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Haunted Story',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Haunted Story'),
       routes: {
         '/second': (context) => SecondPage(),
       },
@@ -41,16 +27,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -58,64 +34,128 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final parser = EmojiParser();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  final List<String> emojiNames = [
+    ':jack_o_lantern:',
+    ':ghost:',
+    ':spider:',
+    ':skull:',
+    ':candy:',
+    ':brain:',
+  ];
+
+  final Random random = Random();
+
+  late List<Offset> positions;
+
+  Timer? timer;
+
+  late AudioCache _audioCache;
+
+  @override
+  void initState() {
+    super.initState();
+    _audioCache = AudioCache(prefix: 'assets/sounds/');
+    positions = List.generate(emojiNames.length, (_) => randomOffset());
+    timer = Timer.periodic(const Duration(seconds: 1), (_) {
+      setState(() {
+        positions = List.generate(emojiNames.length, (_) => randomOffset());
+      });
     });
   }
 
   @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
+  }
+
+  Offset randomOffset() {
+    double x = 0.1 + random.nextDouble() * 0.8;
+    double y = 0.1 + random.nextDouble() * 0.8;
+    return Offset(x, y);
+  }
+
+  void onEmojiTap(String emojiName) {
+    if (emojiName == ':brain:') {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('🧠 You found the brain! You win!')),
+      );
+      Future.delayed(const Duration(milliseconds: 500), () {
+        Navigator.pushNamed(context, '/second');
+      });
+    } else {
+      _audioCache.play('scream.mp3');
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('👻 Wrong emoji! Try again...')),
+      );
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: LayoutBuilder(builder: (context, constraints) {
+        double width = constraints.maxWidth;
+        double height = constraints.maxHeight;
+        return Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/halloween2.jpg"),
+              fit: BoxFit.cover,
             ),
-          ],
-        ),
-      ),
+          ),
+          child: Stack(
+            children: [
+              Positioned(
+                top: 40,
+                left: 0,
+                right: 0,
+                child: Center(
+                  child: Text(
+                    "Click the Brain!",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 28,
+                      shadows: [
+                        Shadow(
+                          blurRadius: 8.0,
+                          color: Colors.black.withOpacity(0.7),
+                          offset: Offset(0, 4),
+                        ),
+                        Shadow(
+                          blurRadius: 12.0,
+                          color: Colors.deepPurple.withOpacity(0.5),
+                          offset: Offset(0, 0),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              ...List.generate(emojiNames.length, (index) {
+                final emojiName = emojiNames[index];
+                final emoji = parser.get(emojiName).code;
+                final pos = positions[index];
+                return AnimatedPositioned(
+                  duration: const Duration(milliseconds: 800),
+                  left: pos.dx * width - 24, // center emoji horizontally
+                  top: pos.dy * height - 24, // center emoji vertically
+                  child: GestureDetector(
+                    onTap: () => onEmojiTap(emojiName),
+                    child: Text(
+                      emoji,
+                      style: const TextStyle(fontSize: 48),
+                    ),
+                  ),
+                );
+              }),
+            ],
+          ),
+        );
+      }),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.pushNamed(context, '/second');
